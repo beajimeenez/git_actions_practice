fa0dee0e2b447bc722a50def47be5b534bac6f1b
beajimeenez <202300419@alu.comillas.edu>
2026-02-04 10:43:21 +0100

    añadir grupo

--- test_servidor.py
+++ test_servidor.py
@@ -35,6 +35,8 @@ class TestSimpleHTTPServer(unittest.TestCase):
         # Verifica que el contenido HTML esperado esté en la respuesta
         self.assertIn("<h1>Hola desde un servidor Python</h1>", response.text)
         self.assertIn("<p>Este es un servidor simple que responde a solicitudes GET.</p>", response.text)
+        self.assertIn("<p>Este es el proyecto del grupo BIKI</p>", response.text)
+        
 
 
 if __name__ == '__main__':
